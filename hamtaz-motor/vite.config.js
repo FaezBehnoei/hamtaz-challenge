06a8b65544284034babd3bcf7f0a6ba649e83c10
Faez Behnoei <faezbehnoei99@gmail.com>
2025-08-08 19:20:04 +0330

    chore: Initialize project with core dependencies and configuration

---
+++ hamtaz-motor/vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    open: '/auth/login',
+    proxy: {
+      '/api': {
+        target: 'https://api.restful-api.dev',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
+})
